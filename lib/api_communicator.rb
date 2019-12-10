b94d9a01952648166ab6183a9c2aa54dc8ca11c8
github username <github email address>
2019-12-10 15:08:53 -0500

    Dean's Version

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -9,20 +9,45 @@ def get_character_movies_from_api(character_name)
 
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+      movie_list = []
+
+  response_hash["results"].each do |character|
+      if character["name"].downcase == character_name #downcase b/c its changed to downcase
+          movie_list = character["films"]  # this is an array
+          puts movie_list
+      end
+    end
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+    movie_info_array = []
+    movie_list.each do |movies|
+    #web request for URL
+    resp_movie = RestClient.get(movies)
+    response_movie_p = JSON.parse(resp_movie)
+
+    movie_info_array << response_movie_p
+
+  end
+
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+    return movie_info_array
 end
 
+
 def print_movies(films)
+  puts "*** Movie List ***"
   # some iteration magic and puts out the movies in a nice list
+  # binding.pry
+films.each {|movies| puts movies["title"]}
 end
 
 def show_character_movies(character)
+  # binding.pry
   films = get_character_movies_from_api(character)
   print_movies(films)
 end
